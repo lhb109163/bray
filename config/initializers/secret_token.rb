315cb16b3414fe0f6430909eae33a04a8740be8c
lhb109163 <lhb109163@163.com>
2014-03-21 14:51:03 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bray::Application.config.secret_key_base = '5428651b3977ddfcaa208ee16ba5d78bbab15f5d232516233361bcdcf415d93064e4c78f71ceff79ed2ecdcb0eb0d50b4f866e30b23a458de92c13258c2f898d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bray::Application.config.session_store :cookie_store, key: '_bray_session'
